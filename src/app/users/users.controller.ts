199dfb8f6bec1bc4efb89af36ef3944818b36919
Jane Doe <jdoe@example.com>
2019-06-07 17:13:16 +0530

    Swagger Implementation

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,21 +1,30 @@
-import { Controller, Get, Request, Param } from '@nestjs/common';
+import { Controller, Get, Request, Param,Post, HttpCode,Header } from '@nestjs/common';
+import { ApiBearerAuth, ApiUseTags, ApiResponse } from '@nestjs/swagger';
 import { UserService } from '@app/users/user.service';
 
-
-@Controller()
+@ApiBearerAuth()
+@ApiUseTags('users')
+@Controller('users')
 export class UsersController {
 
     constructor(private readonly userService: UserService) {}
 
-    @Get('users')
+    @Get()
+    @ApiResponse({ status: 201, description: 'The record has been successfully created.'})
     findAll() {
       return this.userService.getMessage();
     }
 
-    @Get('user/:id')
-    findOne(@Param() params) {
-        console.log(params.id);
-        return this.userService.getMessage(params.id);
+    @Get(':id/:type')
+    findOne(@Param('id') id, @Param('type') type) {
+        console.log(type);
+        console.log(type);
+        return this.userService.getMessage(id);
+    }
+
+    @Post()
+    create() {
+    return 'This action adds a new cat';
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+  .setTitle('Inventory example')
+  .setDescription('The Inventory API description')
+  .setLicense('Apache 2.0','https://www.apache.org/licenses/LICENSE-2.0.html')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, options);
+
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
