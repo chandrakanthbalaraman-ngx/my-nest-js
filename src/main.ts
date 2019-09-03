6c685595d863b7a7b0dac5c8c561c16e19a0ce72
Chandrakanth Balaraman <chandrakanth.ba@object-frontier.com>
2019-09-03 12:06:32 +0530

    Commerce Swagger Doc Changes

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-  .setTitle('Inventory example')
-  .setDescription('The Inventory API description')
+  .setTitle('Commerce')
+  .setDescription('The Commerce API description')
   .setLicense('Apache 2.0','https://www.apache.org/licenses/LICENSE-2.0.html')
   .setVersion('1.0')
   .build();
